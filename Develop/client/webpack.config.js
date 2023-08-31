edf54afaff6d246202b53fe2144490b3c13dc59a
Lavina <129137303+WHT-RBT@users.noreply.github.com>
2023-08-31 16:32:40 -0700

    webpack.config code

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,13 +4,12 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-new InjectManifest({
-  swSrc: './src-sw.js',
-  swDest: 'src-sw.js',
-}),
+// new InjectManifest({
+//   swSrc: './src-sw.js',
+//   swDest: 'src-sw.js',
+// }),
 
 // TODO: Add CSS loaders and babel to webpack.
-
 
 module.exports = () => {
   return {
@@ -24,12 +23,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // InjectManifest for handling service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        // CSS handling
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        // javascript handling - babel
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
     },
   };
